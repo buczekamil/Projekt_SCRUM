b1cfb4ad1ba5c9ec13c543185953367145b5f4e8
Borys Chabior <chabiorborys@gmail.com>
2020-03-30 09:24:37 +0200

    LemonTree

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 
-# Create your models here.
+class Recipe (models.Model):
+    name = models.CharField(max_length=64)
+    ingredients = models.CharField(max_length=300)
+    description = models.TextField()
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    preparation_time = models.IntegerField()
+    votes = models.IntegerField(default=0)
